ba150824539b01b39c15ac891bfea346ca270aa1
Aurélien Hervé <aurelien@aircall.io>
2021-03-11 15:45:34 +0100

    simon game encoded

--- Simon.ino
+++ Simon.ino
@@ -1,7 +1,3 @@
-/*
-  Adafruit Arduino - Lesson 10. Simple Sounds
-*/
-
 //pentatonic scale:
 //  220,
 //  261.6,
@@ -11,76 +7,121 @@
 //  440,
 
 
-int speakerPin = 2;
+const int speakerPin = 2;
 
-int greenIn = 3;
-int blueIn = 4;
-int yellowIn = 5;
-int redIn = 6;
-
-int greenOut = 3;
-int blueOut = 4;
-int yellowOut = 5;
-int redOut = 6;
-
-int input = 0;
-
-int scale[] = {
+// Frequencies in hertz (rounded to int)
+const int scale[] = {
   262,
   294,
   330,
   392,
 };
 
-int colorsIn[] = {
-  greenIn,
-  blueIn,
-  yellowIn,
-  redIn,
+// pins for LEDs
+const int colors[] = {
+  3,
+  4,
+  5,
+  6,
 };
 
-int colorsOut[] = {
-  greenOut,
-  blueOut,
-  yellowOut,
-  redOut,
-};
+// length of max melody
+const int melodyLen = 100;
+int melody[melodyLen]; // init melody array
 
 int scaleLen = sizeof(scale) / sizeof(int);
 
-int melodyLen = 7;
-
-int randomIndex(int scale[], int len) {
-  return floor(random(len));
+int randomIndex() {
+  return floor(random(scaleLen));
 }
 
 void setup() {
   randomSeed(analogRead(0));
-
-  for (int i = 0; i < melodyLen; i++)
-  {
-    play(randomIndex(scale, scaleLen));
-  }
-  noTone(speakerPin);
+  initMelody();
+  blink();
 }
 
 void play(int i) {
-  digitalWrite(colorsOut[i], HIGH);
+  // take control of led pin
+  pinMode(colors[i], OUTPUT);
+  // light LED
+  digitalWrite(colors[i], HIGH);
+
+  // play music
   tone(speakerPin, scale[i]);
   delay(200);
   noTone(speakerPin);
-  digitalWrite(colorsOut[i], LOW);
+
+  // turn off LED
+  digitalWrite(colors[i], LOW);
   delay(100);
+  // give back led pin for reading
+  pinMode(colors[i], INPUT);
 }
 
+int level = 0;
+int cursor = 0;
 void loop() {
-
-  for (int i = 0; i < scaleLen; i++) {
-    input = digitalRead(colorsIn[i]);
-    if (input == HIGH) {
-      play(i);
+  level++;
+  playUpTo(level);
+  cursor = 0;
+  while (cursor < level) {
+    if (getInput() == melody[cursor]) {
+      cursor++;
+    } else {
+      fail();
+      delay(500);
+      break;
     }
   }
+}
 
+void fail() {
+  blink();
+  initMelody();
+  cursor = 0;
+  level = 0;
+}
 
+int getInput() {
+  while(true) {
+    for (int i = 0; i < scaleLen; i++) {
+      if (digitalRead(colors[i])== HIGH) { 
+        return i;
+      }
+    }
+  }
+}
+
+void playUpTo(int iMax) {
+  for (int i = 0; i < iMax ; i++) {
+    play(i);
+  }
+}
+
+void initMelody() {
+  for (int i = 0; i < melodyLen; i++) {
+    melody[i] = randomIndex();
+  }
+}
+
+void blink() {
+  for (int i = 0; i < scaleLen; i++) {
+    pinMode(colors[i], OUTPUT);
+  }
+
+  for (int b = 0; b < 5; b++) {
+    for (int i = 0; i < scaleLen; i++) {
+      digitalWrite(colors[i], HIGH);
+    }
+    delay(300);
+    for (int i = 0; i < scaleLen; i++) {
+      digitalWrite(colors[i], LOW);
+    }
+    delay(300);
+  }
+
+  for (int i = 0; i < scaleLen; i++) {
+    pinMode(colors[i], INPUT);
+  }
 }

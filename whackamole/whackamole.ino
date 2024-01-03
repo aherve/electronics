c2017a19feadd2b5e475accea80b42ab24ec03c3
Aurélien Hervé <mail@aurelien-herve.com>
2024-01-03 22:10:35 +0100

    display led state, display number

--- whackamole/whackamole.ino
+++ whackamole/whackamole.ino
@@ -1,13 +1,14 @@
 // serial communication with the shift register
-#define SHIFT_LATCH 13;
-#define SHIFT_CLOCK 12;
-#define SHIFT_DATA 11;
+#define SHIFT_LATCH 13
+#define SHIFT_CLOCK 12
+#define SHIFT_DATA 11
 
 // pins that can be fed by the multiplexer. Set to low to turn on (and attach a resistor to them)
-const int digitSelectors[4] = {0, 1, 2, 3}; // 4 x7 segment displays
-#define ledSelector 4; // 
+const int digitSelectors[4] = { 0, 1, 2, 3 };  // 4 x7 segment displays
+const int numberSegments[10] = { B00111111, B00000110, B01011011, B01001111, B01100110, B01101101, B01111101, B00000111, B01111111, B01101111 };
 
-const int displayNumber[10] = {B00111111, B00000110, B01011011, B01001111, B01100110, B01101101, B01111101, B00000111, B01111111, B01101111};
+#define ledSelector 4
+bool ledState[5] = {true,false,true,false,true};
 
 void setup() {
   //set pins to output so you can control the shift register
@@ -17,33 +18,58 @@ void setup() {
   // set the selector pins to output, and set to HIGH (off)
   pinMode(ledSelector, OUTPUT);
   digitalWrite(ledSelector, HIGH);
-  // set the digit pins to output, and set to HIGH (off)
+  // set the digit pins to output, and set their selector to HIGH (off)
   for (int digitIndex = 0; digitIndex < 5; digitIndex++) {
-    pinMode(digitPins[digitIndex], OUTPUT);
-    digitalWrite(digitPins[digitIndex], HIGH);
+    pinMode(digitSelectors[digitIndex], OUTPUT);
+    digitalWrite(digitSelectors[digitIndex], HIGH);
   }
 }
 
-lastChange = millis()
-  int value = 0;
-  void loop() {
-    unsigned long now = millis();
-    if (now - lastChange > 500) {
-      value = (value + 1);
-      lastChange = now;
+unsigned long lastChange = millis();
+int value = 0;
+void loop() {
+  unsigned long now = millis();
+  if (now - lastChange > 500) {
+    value++;
+    lastChange = now;
+
+    for (int i = 0; i < 5; i++) {
+      ledState[i] = !ledState[i];
     }
 
-    displayNumber()
-
   }
+
+  displayNumber(value);
+  displayLEDs(ledState);
+}
 
 void displayNumber(int value) {
   for (int digit = 0; digit < 4; digit++) {
-    thisValue = (value / 10^digit) % 10;
-    digitalWrite(digitSelectors[digit], LOW);
+    if (value == 0 && digit > 0) {
+      return;
+    }
     digitalWrite(SHIFT_LATCH, LOW);
-    shiftOut(SHIFT_DATA, SHIFT_CLOCK, MSBFIRST, displayNumber[thisValue]);
+    shiftOut(SHIFT_DATA, SHIFT_CLOCK, MSBFIRST, numberSegments[value % 10]);
     digitalWrite(SHIFT_LATCH, HIGH);
+    digitalWrite(digitSelectors[digit], LOW);
+    delay(1);
     digitalWrite(digitSelectors[digit], HIGH);
+    value = value / 10;
   }
+}
+
+void displayLEDs(bool state[5]) {
+  int asNumber = 0;
+  for (int i = 0; i < 5; i++) {
+    asNumber = asNumber << 1;
+    if (state[i]) {
+      asNumber++;
+    }
+  }
+    digitalWrite(SHIFT_LATCH, LOW);
+    shiftOut(SHIFT_DATA, SHIFT_CLOCK, MSBFIRST, asNumber);
+    digitalWrite(SHIFT_LATCH, HIGH);
+    digitalWrite(ledSelector, LOW);
+    delay(10);
+    digitalWrite(ledSelector, HIGH);
 }

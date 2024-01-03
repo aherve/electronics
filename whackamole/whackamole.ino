4394982229ce64e535f1de66d1046d7f61d7f579
Aurélien Hervé <aurelien@bobsled.co>
2024-01-03 20:04:42 +0100

    playing around

--- whackamole/whackamole.ino
+++ whackamole/whackamole.ino
@@ -1,47 +1,49 @@
 // serial communication with the shift register
-int latchPin = 13;
-int clockPin = 12;
-int dataPin = 11;
+#define SHIFT_LATCH 13;
+#define SHIFT_CLOCK 12;
+#define SHIFT_DATA 11;
 
-int digitPins[5] = {0, 1, 2, 3, 4};
+// pins that can be fed by the multiplexer. Set to low to turn on (and attach a resistor to them)
+const int digitSelectors[4] = {0, 1, 2, 3}; // 4 x7 segment displays
+#define ledSelector 4; // 
 
-int numbers[10] = {B00111111, B00000110, B01011011, B01001111, B01100110, B01101101, B01111101, B00000111, B01111111, B01101111};
+const int displayNumber[10] = {B00111111, B00000110, B01011011, B01001111, B01100110, B01101101, B01111101, B00000111, B01111111, B01101111};
 
 void setup() {
   //set pins to output so you can control the shift register
-  pinMode(latchPin, OUTPUT);
-  pinMode(clockPin, OUTPUT);
-  pinMode(dataPin, OUTPUT);
+  pinMode(SHIFT_LATCH, OUTPUT);
+  pinMode(SHIFT_CLOCK, OUTPUT);
+  pinMode(SHIFT_DATA, OUTPUT);
+  // set the selector pins to output, and set to HIGH (off)
+  pinMode(ledSelector, OUTPUT);
+  digitalWrite(ledSelector, HIGH);
+  // set the digit pins to output, and set to HIGH (off)
   for (int digitIndex = 0; digitIndex < 5; digitIndex++) {
     pinMode(digitPins[digitIndex], OUTPUT);
     digitalWrite(digitPins[digitIndex], HIGH);
   }
-  int digitIndex = 0;
 }
-int numberToDisplay = 0;
 
-unsigned long lastChange = millis();
+lastChange = millis()
+  int value = 0;
+  void loop() {
+    unsigned long now = millis();
+    if (now - lastChange > 500) {
+      value = (value + 1);
+      lastChange = now;
+    }
 
-void loop() {
-  // count from 0 to 255 and display the number
-  // on the LEDs
+    displayNumber()
 
-  unsigned long now = millis();
-  if (now - lastChange > 500) {
-    numberToDisplay = (numberToDisplay + 1 ) % 10;
-    lastChange = now;
   }
 
-  for (int pinIndex = 0; pinIndex < 5; pinIndex++) {
-
-    int pin = digitPins[pinIndex];
-    digitalWrite(pin, LOW);
-    digitalWrite(latchPin, LOW);
-    // shift out the bits:
-    shiftOut(dataPin, clockPin, MSBFIRST, numbers[numberToDisplay]);
-    //take the latch pin high so the LEDs will light up:
-    digitalWrite(latchPin, HIGH);
-    // pause before next value:
-    digitalWrite(pin, HIGH);
+void displayNumber(int value) {
+  for (int digit = 0; digit < 4; digit++) {
+    thisValue = (value / 10^digit) % 10;
+    digitalWrite(digitSelectors[digit], LOW);
+    digitalWrite(SHIFT_LATCH, LOW);
+    shiftOut(SHIFT_DATA, SHIFT_CLOCK, MSBFIRST, displayNumber[thisValue]);
+    digitalWrite(SHIFT_LATCH, HIGH);
+    digitalWrite(digitSelectors[digit], HIGH);
   }
 }

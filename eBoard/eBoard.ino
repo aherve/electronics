183ead7ceba665701c681fe66faa2967447b75da
Aurélien Hervé <aurelien@bobsled.co>
2024-03-26 09:29:21 +0100

    cleanup

--- eBoard/eBoard.ino
+++ eBoard/eBoard.ino
@@ -23,19 +23,20 @@ void setup() {
     pinMode(hallLatchPin, OUTPUT);
     digitalWrite(hallLatchPin, LOW);
     digitalWrite(ledLatchPin, LOW);
+    digitalWrite(clockPin, LOW);
   }
   Serial.begin(9600);
 }
 
 bool readBoard() {
   int changed = false;
-  int shiftNumber = 1;
-  for (int i = 0; i < boardSize; i++) {
-    shiftOut(dataPin, clockPin, MSBFIRST, shiftNumber);
+  byte iByte = 1;
+  for (byte i = 0; i < boardSize; i++) {
+    shiftOut(dataPin, clockPin, MSBFIRST, iByte);
     digitalWrite(hallLatchPin, HIGH);
     delay(1);
     digitalWrite(hallLatchPin, LOW);
-    for (int j = 0; j < boardSize; j++) {
+    for (byte j = 0; j < boardSize; j++) {
       int read = analogRead(readPins[j]);
       if (read < downTres) {
         read = -1;
@@ -49,30 +50,30 @@ bool readBoard() {
         boardState[i][j] = read;
       }
     }
-    shiftNumber = shiftNumber << 1;
+    iByte <<= 1;
   }
   return changed;
 }
 
 void displayLEDs() {
-  int iByte = 1;
-  for (int i = 0; i < boardSize; i++) {
+  byte iByte = 1;
+  for (byte i = 0; i < boardSize; i++) {
     bool hasValue = false;
-    for (int j = 0; j < boardSize; j++) {
+    for (byte j = 0; j < boardSize; j++) {
       if (ledState[i][j]) {
         hasValue = true;
         break;
       }
     }
     if (!hasValue) {
-      iByte = iByte << 1;
+      iByte <<= 1;
       continue;
     }
 
-    int jByte = 0;
-    for (int j = 0; j < boardSize; j++) {
-      jByte = jByte << 1;
-      if (!ledState[i][j]) {
+    byte jByte = 0;
+    for (byte j = 0; j < boardSize; j++) {
+      jByte <<= 1;
+      if (!ledState[i][j]) { // 0 to enable
         jByte++;
       }
     }
@@ -84,7 +85,7 @@ void displayLEDs() {
     digitalWrite(ledLatchPin, HIGH);
     digitalWrite(ledLatchPin, LOW);
 
-    iByte = iByte << 1;
+    iByte <<= 1;
   }
 }
 
@@ -92,12 +93,12 @@ unsigned long lastLedChange = 0;
 void loop() {
 
   if (readBoard()) {
-    display(boardState);
+    displayBoard();
   }
 
   if ((millis() - lastLedChange) > 1000) {
-    for (int i = 0; i < boardSize; i++) {
-      for (int j = 0; j < boardSize; j++) {
+    for (byte i = 0; i < boardSize; i++) {
+      for (byte j = 0; j < boardSize; j++) {
         ledState[i][j] = !ledState[i][j];
       }
     }
@@ -107,7 +108,7 @@ void loop() {
   displayLEDs();
 }
 
-void display(int boardState[boardSize][boardSize]) {
+void displayBoard() {
   Serial.println("--------");
   for (int i = 0; i < boardSize; i++) {
     for (int j = 0; j < boardSize; j++) {
